043024e363af1c5cf6653da45c692152555dec27
EC2 Default User <ec2-user@ip-172-31-40-81.us-west-2.compute.internal>
2018-05-05 16:27:12 +0000

    Featured Image

---
+++ src/.vuepress/components/FeaturedImage.vue
@@ -0,0 +1,26 @@
+<!-- 
+Component FeaturedImage
+Usage:
+  <FeaturedImage caption="Caption Text" src="/path/to/img" />
+-->
+
+<template>
+  <div
+    class="custom-block tip"
+    style="float:right; margin-left:4px; width:30%;"
+  >
+    
+    <img
+      style="margin-top:8px"
+      v-bind:src="src"
+    />
+    
+    <p>{{ caption }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['src', 'caption']
+  };
+</script>
---
+++ src/.vuepress/components/PageDataTest.vue
@@ -0,0 +1,17 @@
+<!--
+Component PageDataTest
+Usage:
+  <PageDataText :page="$page" />
+-->
+
+<template>
+  <div>
+    <p>This is the page data:</p> {{ page }}
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['page']
+  };
+</script>
