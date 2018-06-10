c1fd3306587df693aac016fb43687bc6bfb6e5e6
EC2 Default User <ec2-user@ip-172-31-11-24.us-west-2.compute.internal>
2018-06-10 17:10:47 +0000

    YouTubeVideo component

---
+++ src/.vuepress/components/YouTubeVideo.vue
@@ -0,0 +1,17 @@
+
+<template>
+  <iframe
+    width="560"
+    height="315"
+    :src="'https://www.youtube.com/embed/' + videoId"
+    frameborder="0"
+    allow="autoplay; encrypted-media"
+    allowfullscreen>
+  </iframe>
+</template>
+
+<script>
+export default {
+  props: ['videoId']
+}
+</script>
